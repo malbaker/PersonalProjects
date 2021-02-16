26f3bc3c3064f24b172726cced46c9e76af518ff
Malik Baker <mbaker@fedora.bu.edu>
2021-02-16 11:33:50 -0500

    rubystuff

---
+++ rubytest/first.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+
+=begin
+puts "Who tf are you"
+name = gets.chomp
+puts "Sup #{name}"
+=end
+
+
+first_name = ARGV[0]
+last_name = ARGV[1]
+age = ARGV[2]
+
+puts "#{ARGV}"
+
+puts "Hello #{first_name} #{last_name}! I see you are #{age} years of age."
+
+
